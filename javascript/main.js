f5d7fbf555f81bbbb883e6e573d5d00cf98ada4a
victorabedi <iamvictorabedi@gmail.com>
2022-03-01 12:31:18 +0300

    Synced Devons Changes and added dark mode

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,12 @@
 function calculateGrossSalary() {
-    var base = document.getElementById('basesalary');
+    const base = document.getElementById('basesalary');
+    //baseSalary
+    const baseSalary = parseInt(base.value);
     // House allowance is 10% of base salary
-    var houseAllowance = 0.1 * base.value;
+    const houseAllowance = 0.1 * baseSalary;
     // Transport allowance is fixed for all employees
-    const TRANSPORT_ALLOWANCE = 5000;
-    var gross =  base.value + houseAllowance + TRANSPORT_ALLOWANCE;
+    const transportAllowance = 5000;
+    const gross =  (baseSalary +  houseAllowance + transportAllowance);
+    // Map gross value to answer field
+    document.getElementById('answer').innerHTML = gross.toString();
 }
